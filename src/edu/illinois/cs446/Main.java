d692bd62ee70c550dbeec2ce0d745c5c81b9cde4
Brad Misik <temugen@bradmisik.com>
2011-11-29 13:20:54 -0600

    Sending vector

--- src/edu/illinois/cs446/Main.java
+++ src/edu/illinois/cs446/Main.java
@@ -3,19 +3,54 @@ package edu.illinois.cs446;
 import java.io.IOException;
 
 public class Main {
+	
+	private static int initial_matrix[][] = {{3, 1}, {5, 2}};
+	private static int initial_vector[] = {2, 7};
 
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		Network network;
-		if(args.length > 1)
-			network = new Client(args[0], new Integer(args[1]));
-		else
-			network = new Server(new Integer(args[0]));
+		boolean done = false;
+		
+		int matrix[][] = null;
+		int vector[] = null;
 	
-		network.write("Hello");
-		System.out.println(network.read());
+		Network network;
+		if(args.length > 1) {
+			network = new Client(args[0], new Integer(args[1]));
+		}
+		else {
+			network = new Server(new Integer(args[0]));
+			
+			vector = initial_vector;
+			
+			//Sending initial data
+			network.write("vector");
+			network.write(new Integer(initial_vector.length).toString());
+			for(Integer num : initial_vector) {
+				network.write(num.toString());
+			}
+			
+			done = true;
+		}
+
+		while(!done) {
+			String line = network.read();
+			System.out.println(line);
+			
+			if(line.equals("vector")) {
+				int count = new Integer(network.read());
+				vector = new int[count];
+				for(int i = 0; i < count; i++) {
+					vector[i] = new Integer(network.read());
+				}
+			}
+		}
+		
+		for(int i = 0; i < vector.length; i++) {
+			System.out.println(new Integer(vector[i]).toString());
+		}
 	}
 }
