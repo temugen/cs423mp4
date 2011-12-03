c42055a4cb9cfeec46b632b2051f38c6f8b892e8
Brad Misik <temugen@bradmisik.com>
2011-12-02 23:12:44 -0600

    still debugging

--- src/edu/illinois/cs446/Main.java
+++ src/edu/illinois/cs446/Main.java
@@ -21,6 +21,7 @@ public class Main {
 		network = new Client(host, port);
 		network.write("pixels");
 		network.write(new Integer(split[1].capacity()).toString());
+		System.out.println(new Integer(split[1].capacity()).toString());
 		split[1].position(0);
 		for(int i = 0; i < split[1].capacity(); i++) {
 			network.write(Integer.toString(split[1].get(), 64));
@@ -51,6 +52,7 @@ public class Main {
 			if(line.equals("pixels")) {
 				int count = new Integer(network.read());
 				IntBuffer buffer = IntBuffer.allocate(count);
+				System.out.println(count + " pixels");
 				for(int i = 0; i < count; i++) {
 					String base64 = network.read();
 					buffer.put(Integer.parseInt(base64, 64));
